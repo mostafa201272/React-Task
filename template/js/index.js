d2acf68550dec560c10df0339b9c7419459ec25d
Mostafa Mahmoud <mostafa201272@yahoo.com>
2023-01-27 20:27:36 +0200

    Add main layout

--- template/js/index.js
+++ template/js/index.js
@@ -6,7 +6,29 @@ $(function(){
         // Fade out the splash screen
         $("#splash-screen").fadeOut();
 
-    }, 5000)
+    }, 100)
+
+    
+
+    // Open Home Page
+    $(document).on("click", "#page-icon-home-page", function(){
+        $(this).addClass('active').siblings().removeClass('active')
+        $("#home-page").fadeIn().siblings().fadeOut()
+    });
+
+    // Open Create Cutomer Page
+    $(document).on("click", "#create-customer-page-link", function(){
+        $("#create-customer-page").fadeIn().siblings().fadeOut()
+        $('#page-icon-create-customer-page').addClass('active').siblings().removeClass('active')
+    });
+    
+    // Open Create Cutomer Page
+    $(document).on("click", "#page-icon-create-customer-page", function(){
+        $(this).addClass('active').siblings().removeClass('active')
+        $("#create-customer-page").fadeIn().siblings().fadeOut()
+    });
+
+    
 
 });
 
