3b2fff5ce5a50af446ea05987784289cf93fcbb7
Mostafa Mahmoud <mostafa201272@yahoo.com>
2023-01-27 16:16:01 +0200

    Update

---
+++ template/js/index.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    // Hide Splash SCREEN afte 3 seconds
+    setTimeout(function(){
+
+        // Fade out the splash screen
+        $("#splash-screen").fadeOut();
+
+    }, 5000)
+
+});
+
